0d54d49bf741892ae8ee236bb45d09bb738f7a61
jamesmcdonagh <james.mcdonagh@statera.com>
2018-05-03 22:45:38 -0600

    Added alternative method

--- Basic/Invoke_Itunes_Tracklist.ps1
+++ Basic/Invoke_Itunes_Tracklist.ps1
@@ -1,2 +1,6 @@
 ﻿$Json = Invoke-WebRequest -Uri "http://itunes.apple.com/search?term=systemofadown" 
 ($Json | ConvertFrom-Json).Results | Select WrapperType, trackName | Select -First 25
+
+#OR
+#$Json = Invoke-RestMethod -Uri "http://itunes.apple.com/search?term=systemofadown"
+#$Json.results | Select WrapperType , trackName | Select -First 25 
